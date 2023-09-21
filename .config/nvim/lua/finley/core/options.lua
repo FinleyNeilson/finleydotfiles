d93c837a0b62f60c2e2ca67f45107837653aa18f
FinleyNeilson <87588037+FinleyNeilson@users.noreply.github.com>
2023-09-21 22:28:58 +1200

    added blankline and some options failed to get lsp to format java

--- .config/nvim/lua/finley/core/options.lua
+++ .config/nvim/lua/finley/core/options.lua
@@ -5,10 +5,12 @@ opt.relativenumber = true -- show relative line numbers
 opt.number = true -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
+opt.smartindent = true
 
 -- line wrapping
 opt.wrap = false -- disable line wrapping
@@ -16,17 +18,19 @@ opt.wrap = false -- disable line wrapping
 -- search settings
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+opt.hlsearch = false
+opt.incsearch = true
 
 -- cursor line
 opt.cursorline = true -- highlight the current cursor line
 
+opt.scrolloff = 8
+
 -- appearance
 
--- turn on termguicolors for nightfly colorscheme to work
--- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
-opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+opt.background = "dark"
+opt.signcolumn = "yes"
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
@@ -38,5 +42,8 @@ opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
--- turn off swapfile
 opt.swapfile = false
+--opt.undodir will add this eventually omg it fked me before
+--opt.undofile = true
+
+opt.colorcolumn = "80"
